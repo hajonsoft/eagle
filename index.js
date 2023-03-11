6122378635b5d2fe2c8111e043c3a1c3b6e7246d
Babatunde Ololade <babatundeololade50@gmail.com>
2023-03-12 00:45:16 +0300

    initial files

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 #!/usr/bin/env node
-require("dotenv").config();
-const pjson = require("./package.json");
+require('dotenv').config();
+const pjson = require('./package.json');
 
 const version = pjson.version;
 console.log(`=== Eagle v${version} ===`);
 
-const { send: sendEhj } = require("./src/ehj");
-const { send: sendBau } = require("./src/bau");
-const { send: sendWtu } = require("./src/wtu");
-const { send: sendGma } = require("./src/gma");
-const { send: sendVst } = require("./src/vst");
-const { send: sendEnj } = require("./src/enj");
-const { send: sendTwf } = require("./src/twf");
-const { send: sendHsf } = require("./src/hsf");
-const { send: sendSfi } = require("./src/sfi");
-const { send: sendSbr } = require("./src/sbr");
-const { send: sendMtf } = require("./src/mtf");
-const { send: sendGhb } = require("./src/ghb");
-const { send: sendNsk } = require("./src/nsk");
+const { send: sendEhj } = require('./src/ehj');
+const { send: sendBau } = require('./src/bau');
+const { send: sendWtu } = require('./src/wtu');
+const { send: sendGma } = require('./src/gma');
+const { send: sendVst } = require('./src/vst');
+const { send: sendEnj } = require('./src/enj');
+const { send: sendTwf } = require('./src/twf');
+const { send: sendHsf } = require('./src/hsf');
+const { send: sendSfi } = require('./src/sfi');
+const { send: sendSbr } = require('./src/sbr');
+const { send: sendMtf } = require('./src/mtf');
+const { send: sendGhb } = require('./src/ghb');
+const { send: sendNsk } = require('./src/nsk');
+const { send: sendTrk } = require('./src/trk');
 
-const util = require("./src/util");
+const util = require('./src/util');
 const { getPath } = util;
-const path = require("path");
-const Cryptr = require("cryptr");
-const fs = require("fs");
-const axios = require("axios");
-const extract = require("extract-zip");
-const { homedir } = require("os");
-const moment = require("moment");
-const budgie = require("./src/budgie");
-const kea = require("./src/lib/kea");
-const inquirer = require("inquirer");
-const defaultSMSAPIKeyMustOverride = "88fd2e1A3f4d327740A9408c12872A39";
+const path = require('path');
+const Cryptr = require('cryptr');
+const fs = require('fs');
+const axios = require('axios');
+const extract = require('extract-zip');
+const { homedir } = require('os');
+const moment = require('moment');
+const budgie = require('./src/budgie');
+const kea = require('./src/lib/kea');
+const inquirer = require('inquirer');
+const defaultSMSAPIKeyMustOverride = '88fd2e1A3f4d327740A9408c12872A39';
 
 let userInput;
 let data = readDataFile();
@@ -40,13 +41,13 @@ async function main() {
   console.log(
     `https://hajonsoft-kea.web.app/admin/submissions/${
       process.argv
-        .find((arg) => arg.startsWith("--submissionId"))
-        ?.split("=")?.[1]
+        .find((arg) => arg.startsWith('--submissionId'))
+        ?.split('=')?.[1]
     }/edit`
   );
 
-  if (process.argv.includes("-v")) {
-    console.log("version: " + version);
+  if (process.argv.includes('-v')) {
+    console.log('version: ' + version);
     process.exit(0);
   }
 
@@ -54,25 +55,25 @@ async function main() {
   try {
     await kea.init();
   } catch (error) {
-    console.error("Error: " + error);
+    console.error('Error: ' + error);
   }
 
   // Cleanups
-  const addModeFile = getPath("add.json");
+  const addModeFile = getPath('add.json');
   if (fs.existsSync(addModeFile)) {
     fs.unlinkSync(addModeFile);
   }
 
-  const loopFile = getPath("loop.txt");
+  const loopFile = getPath('loop.txt');
   if (fs.existsSync(loopFile)) {
     fs.unlinkSync(loopFile);
   }
 
-  if (process.argv.includes("-i")) {
+  if (process.argv.includes('-i')) {
     return runInteractive();
   }
 
-  if (process.argv.includes("budgie")) {
+  if (process.argv.includes('budgie')) {
     printBudgie();
   }
 
@@ -80,10 +81,10 @@ async function main() {
 }
 
 function printBudgie() {
-  const colonSeparatedKeys = process.argv.filter((arg) => arg.includes(":"));
+  const colonSeparatedKeys = process.argv.filter((arg) => arg.includes(':'));
   if (colonSeparatedKeys && colonSeparatedKeys.length > 0) {
     colonSeparatedKeys.forEach((colonSeparatedKey) => {
-      const [key, value] = colonSeparatedKey.split(":");
+      const [key, value] = colonSeparatedKey.split(':');
       budgie.save(key, value);
     });
   }
@@ -96,33 +97,33 @@ function printBudgie() {
 
 async function sendToCloud(data) {
   util.setSelectedTraveller(0);
-  data.info.caravan = "CLOUD_" + data.info.caravan;
+  data.info.caravan = 'CLOUD_' + data.info.caravan;
   data.info.range = process.argv
-    .find((arg) => arg.toLowerCase().startsWith("range"))
+    .find((arg) => arg.toLowerCase().startsWith('range'))
     ?.substring(6)
-    ?.replace(",", "-");
-  data.info.stamp = moment().format("YYYY-MM-DD hh:mm:ss a");
-  fs.writeFileSync(getPath("data.json"), JSON.stringify(data));
+    ?.replace(',', '-');
+  data.info.stamp = moment().format('YYYY-MM-DD hh:mm:ss a');
+  fs.writeFileSync(getPath('data.json'), JSON.stringify(data));
   const command = `git add . && git commit -m "${data.system?.country?.code} ${
     data.travellers?.length
   } Pax ${data.system?.name} ${data.system?.username}.0${moment().format(
-    "mm"
+    'mm'
   )} ${
     process.argv
-      .find((arg) => arg.toLowerCase().startsWith("range"))
-      ?.replace(",", "-") ?? ""
+      .find((arg) => arg.toLowerCase().startsWith('range'))
+      ?.replace(',', '-') ?? ''
   }" && git push origin main:job --force`;
-  const childProcess = require("child_process");
+  const childProcess = require('child_process');
 
   childProcess.exec(command, function (error, stdout, stderr) {
     if (error) {
-      console.log("Eagle Cloud Error: " + error.code);
+      console.log('Eagle Cloud Error: ' + error.code);
     }
     if (stdout) {
-      console.log("Eagle Cloud: " + stdout);
+      console.log('Eagle Cloud: ' + stdout);
     }
     if (stderr) {
-      console.log("Eagle cloud: " + stderr);
+      console.log('Eagle cloud: ' + stderr);
     }
   });
   // sendGhb(data);
@@ -130,7 +131,7 @@ async function sendToCloud(data) {
 
 async function submitToProvider() {
   const dataFileName = await getDataFileName();
-  const content = fs.readFileSync(dataFileName, "utf8");
+  const content = fs.readFileSync(dataFileName, 'utf8');
   const data = JSON.parse(content);
   if (!data.info.run) {
     data.info.run = 0;
@@ -149,52 +150,54 @@ async function submitToProvider() {
   //   }
   // }
   console.log(
-    "\x1b[32m",
+    '\x1b[32m',
     `starting process ...[${data.system.name} ${
       data.travellers.length
     } PAX => ${util.getSelectedTraveler()}]`,
-    "\x1b[0m"
+    '\x1b[0m'
   );
   const lastIndex = util.getSelectedTraveler();
   if (lastIndex >= data.travellers.length) {
     util.setSelectedTraveller(0);
   }
 
-  if (process.argv.includes("-cloud") || process.argv.includes("-submit")) {
+  if (process.argv.includes('-cloud') || process.argv.includes('-submit')) {
     return sendToCloud(data);
   }
 
   switch (data.system.name) {
-    case "ehj":
+    case 'ehj':
       return sendEhj(data);
-    case "bau":
+    case 'bau':
       return sendBau(data);
-    case "wtu":
+    case 'trk':
+      return sendTrk(data);
+    case 'wtu':
       return sendWtu(data);
-    case "ghb":
+    case 'ghb':
       return sendGhb(data);
-    case "gma":
+    case 'gma':
       return sendGma(data);
-    case "twf":
+    case 'twf':
       return sendTwf(data);
     // case "mot":
     //   return sendMot(data);
-    case "vst":
+    case 'vst':
       return sendVst(data);
-    case "enj":
+    case 'enj':
       return sendEnj(data);
-    case "hsf":
+    case 'hsf':
       return sendHsf(data);
-    case "sfi":
+    case 'sfi':
       return sendSfi(data);
-    case "sbr":
+    case 'sbr':
       return sendSbr(data);
-    case "mtf":
+    case 'mtf':
       return sendMtf(data);
-      case "nsk":
-        return sendNsk(data);
+    case 'nsk':
+      return sendNsk(data);
     default:
-      console.log("unknown system");
+      console.log('unknown system');
   }
 }
 
@@ -204,9 +207,9 @@ async function unzipFile(source, outputDir) {
     const files = fs.readFileSync(dir);
     for (const file of files.filter(
       (file) =>
-        file.endsWith("_photo.jpg") ||
-        file.endsWith("_mrz.jpg") ||
-        file.endsWith("_passport.jpg")
+        file.endsWith('_photo.jpg') ||
+        file.endsWith('_mrz.jpg') ||
+        file.endsWith('_passport.jpg')
     )) {
       fs.unlinkSync(path.join(dir, file));
     }
@@ -221,45 +224,45 @@ async function unzipFile(source, outputDir) {
 
 async function getDataFileName() {
   const fileParam = process.argv.find((arg) =>
-    arg.toLowerCase().startsWith("file")
+    arg.toLowerCase().startsWith('file')
   );
 
-  let dataFileName = getPath("data.json");
+  let dataFileName = getPath('data.json');
 
   if (fileParam) {
     let fileName = fileParam.substring(5);
     if (!fileName) {
-      fileName = "bundle.zip";
+      fileName = 'bundle.zip';
     }
     if (/^[0-9]$/.test(fileName)) {
-      fileName = "bundle (" + fileName + ").zip";
+      fileName = 'bundle (' + fileName + ').zip';
     }
-    if (!fileName.endsWith(".zip")) {
-      fileName = fileName + ".zip";
+    if (!fileName.endsWith('.zip')) {
+      fileName = fileName + '.zip';
     }
-    if (!fileParam.includes("/") && !fileParam.includes("\\")) {
+    if (!fileParam.includes('/') && !fileParam.includes('\\')) {
       //Only file name was provided: Assume default download folder
       fileName = path.join(getDownloadFolder(), fileName);
-      console.log("FILE NAME: ", fileName);
+      console.log('FILE NAME: ', fileName);
     }
     if (!fs.existsSync(fileName)) {
       console.log(`File not found ${fileName}`);
       process.exit(1);
     }
 
-    const outputDir = dataFileName.replace("data.json", "");
+    const outputDir = dataFileName.replace('data.json', '');
     await unzipFile(fileName, outputDir);
 
     console.log(
-      "\x1b[7m",
+      '\x1b[7m',
       `Success: ${fileName} => ${outputDir}/data.json`,
-      "\x1b[0m"
+      '\x1b[0m'
     );
   }
 
   if (!fs.existsSync(dataFileName)) {
-    if (fs.existsSync(path.join(__dirname, "./data.json"))) {
-      fs.copyFileSync(path.join(__dirname, "./data.json"), dataFileName);
+    if (fs.existsSync(path.join(__dirname, './data.json'))) {
+      fs.copyFileSync(path.join(__dirname, './data.json'), dataFileName);
       return dataFileName;
     }
 
@@ -270,23 +273,23 @@ async function getDataFileName() {
 }
 
 function readDataFile() {
-  if (fs.existsSync(getPath("data.json"))) {
-    return JSON.parse(fs.readFileSync(getPath("data.json"), "utf-8"));
+  if (fs.existsSync(getPath('data.json'))) {
+    return JSON.parse(fs.readFileSync(getPath('data.json'), 'utf-8'));
   }
-  if (fs.existsSync(path.join(__dirname, "data.json"))) {
+  if (fs.existsSync(path.join(__dirname, 'data.json'))) {
     return JSON.parse(
-      fs.readFileSync(path.join(__dirname, "data.json"), "utf-8")
+      fs.readFileSync(path.join(__dirname, 'data.json'), 'utf-8')
     );
   }
-  console.error("NO data.json found");
+  console.error('NO data.json found');
 }
 
 function runInteractive() {
   readDataFile();
-  let currentSlug = data?.travellers?.[0]?.slug || "unknown Slug";
+  let currentSlug = data?.travellers?.[0]?.slug || 'unknown Slug';
   setTimeout(() => {
     if (userInput) {
-      return console.log("userInput", userInput);
+      return console.log('userInput', userInput);
     }
     process.exit(0);
   }, 15000);
@@ -294,72 +297,72 @@ function runInteractive() {
   inquirer
     .prompt([
       {
-        name: "action",
-        message: "Advanced options!",
-        type: "list",
+        name: 'action',
+        message: 'Advanced options!',
+        type: 'list',
         choices: [
           `1- Run eagle default file ${currentSlug}`,
-          "2- Run Budgie display",
-          "3- Update Budgie... [will prompt]",
+          '2- Run Budgie display',
+          '3- Update Budgie... [will prompt]',
           `4- Set download folder. [${getDownloadFolder()}]`,
-          "5- Get SMS number",
-          "0- Exit",
+          '5- Get SMS number',
+          '0- Exit',
         ],
       },
     ])
     .then((answers) => {
       userInput = answers.action;
-      if (answers.action.startsWith("1-")) {
+      if (answers.action.startsWith('1-')) {
         return submitToProvider();
       }
 
-      if (answers.action.startsWith("2-")) {
+      if (answers.action.startsWith('2-')) {
         return printBudgie();
       }
-      if (answers.action.startsWith("3-")) {
-        return console.log("not implemented, try node . budgie=KEY:VALUE");
+      if (answers.action.startsWith('3-')) {
+        return console.log('not implemented, try node . budgie=KEY:VALUE');
       }
-      if (answers.action.startsWith("4-")) {
+      if (answers.action.startsWith('4-')) {
         inquirer
           .prompt({
-            type: "input",
+            type: 'input',
             message: `Download folder: ${getDownloadFolder()}`,
-            name: "downloadFolder",
+            name: 'downloadFolder',
           })
           .then((answers) => {
             if (answers.downloadFolder) {
               fs.writeFileSync(
-                getPath(".downloadFolder"),
+                getPath('.downloadFolder'),
                 answers.downloadFolder
               );
             }
           });
         return;
       }
-      if (answers.action.startsWith("5-")) {
+      if (answers.action.startsWith('5-')) {
         return runGetSMSNumber();
       }
-      if (answers.action.startsWith("0-")) {
+      if (answers.action.startsWith('0-')) {
         process.exit(0);
       }
     });
 }
 
 function getDownloadFolder() {
-  if (fs.existsSync(getPath(".downloadFolder"))) {
-    return fs.readFileSync(getPath(".downloadFolder"), "utf-8");
+  if (fs.existsSync(getPath('.downloadFolder'))) {
+    return fs.readFileSync(getPath('.downloadFolder'), 'utf-8');
   }
-  return path.join(homedir(), "Downloads");
+  return path.join(homedir(), 'Downloads');
 }
 
-process.on("uncaughtException", function (error) {
-  util.infoMessage(null, "uncaughtException: " + error);
-  util.infoMessage(null, "exit error code 1");
+process.on('uncaughtException', function (error) {
+  util.infoMessage(null, 'uncaughtException: ' + error);
+  util.infoMessage(null, 'exit error code 1');
   process.exit(1);
 });
 
-process.on("unhandledRejection", function (reason, p) {
-  util.infoMessage(null, "unhandledRejection: " + reason, p);
+process.on('unhandledRejection', function (reason, p) {
+  util.infoMessage(null, 'unhandledRejection: ' + reason, p);
 });
 
 main();
---
+++ src/trk.js
@@ -0,0 +1,407 @@
+const puppeteer = require('puppeteer-extra');
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const fs = require('fs');
+const path = require('path');
+const util = require('./util');
+const { getPath } = util;
+const sharp = require('sharp');
+const totp = require('totp-generator');
+
+let page;
+let data;
+let counter = 0;
+
+function getLogFile() {
+  const logFolder = path.join(getPath('log'), data.info.munazim);
+  if (!fs.existsSync(logFolder)) {
+    fs.mkdirSync(logFolder, { recursive: true });
+  }
+  const logFile = path.join(logFolder, data.info.caravan + '_ehj.txt');
+  return logFile;
+}
+
+let startTime;
+
+const config = [
+  {
+    name: 'result_print_renewal',
+    url: 'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris/UzatmaBasvuru',
+    regex:
+      'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris/UzatmaBasvuru',
+    details: [
+      {
+        selector: '#basvuruNo',
+        value: () => 'application No',
+      },
+      {
+        selector: '#cepTelefon',
+        value: 'cell phone',
+      },
+      {
+        selector: '#ePosta',
+        value: 'Email Address',
+      },
+      {
+        selector: '#yabanciKimlikNo',
+        value: 'Foreign Number ID',
+      },
+      {
+        selector: '#pasaportBelgeNo',
+        value: 'passport number',
+      },
+    ],
+  },
+  {
+    name: 'continue_application_renewal',
+    url: 'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris/UzatmaBasvuru',
+    regex:
+      'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris/UzatmaBasvuru',
+    details: [
+      {
+        selector: '#basvuruNo',
+        value: () => 'application No',
+      },
+      {
+        selector: '#cepTelefon',
+        value: 'cell phone',
+      },
+      {
+        selector: '#ePosta',
+        value: 'Email Address',
+      },
+      {
+        selector: '#yabanciKimlikNo',
+        value: 'Foreign Number ID',
+      },
+      {
+        selector: '#pasaportBelgeNo',
+        value: 'passport number',
+      },
+    ],
+  },
+  {
+    name: 'apply_renewal',
+    url: 'https://e-ikamet.goc.gov.tr/Ikamet/UzatmaGecisGiris/UzatmaBasvuru',
+    regex: 'https://e-ikamet.goc.gov.tr/Ikamet/UzatmaGecisGiris/UzatmaBasvuru',
+    details: [
+      {
+        selector:
+          '#body > div > div > section > div > div.content--box--detail > div > div > div.form-horizontal.col-lg-12 > div > div.col-lg-8.controls > span > span > span.k-input',
+        value: () => '',
+      },
+      {
+        selector: '#ad',
+        value: () => 'AB',
+      },
+      {
+        selector: '#soyad',
+        value: () => 'AC',
+      },
+      {
+        selector: '#yabanciKimlikNo',
+        value: () => '8493934',
+      },
+      {
+        selector: '#belgeSeri',
+        value: () => 'Residence Permit card serial No.',
+      },
+      {
+        selector: '#belgeNo',
+        value: () => 'Residence Permit Card No.',
+      },
+      {
+        selector:
+          '#body > div > div > section > div > div.content--box--detail > div > div > div:nth-child(5) > div:nth-child(3) > div.col-lg-8.controls > span > span > span.k-input',
+        value: () => 'Country of Nationality',
+      },
+      {
+        selector:
+          '#body > div > div > section > div > div.content--box--detail > div > div > div:nth-child(10) > div.form-horizontal.col-md-6.col-centered > div > div.col-lg-8.controls > span > span > span.k-input',
+        value: () => 'Communication Preference',
+      },
+      {
+        selector: '#iletisim_eMail',
+        value: () => 'Email Address',
+      },
+      {
+        selector: '#iletisim_cepTelefon',
+        value: () => 'Cell Phone',
+      },
+      {
+        selector: '#uzatmaGecisOkudumAnladim',
+        value: () => 'Read and Understood',
+      },
+    ],
+  },
+  {
+    name: 'apply',
+    url: 'https://e-ikamet.goc.gov.tr/Ikamet/OnKayit',
+    regex: 'https://e-ikamet.goc.gov.tr/Ikamet/OnKayit',
+    details: [
+      {
+        selector: '#ad',
+        // value: (row) => row.name.first,
+        value: (row) => 'Ololade',
+      },
+      {
+        selector: '#soyad',
+        // value: (row) => row.name.last,
+        value: (row) => 'Owolabi',
+      },
+      {
+        selector: '#DogumTarih',
+        // value: (row) => row.dob.dmy,
+        value: (row) => '01/01/1981',
+      },
+      {
+        selector: '#babaAd',
+        // value: (row) => row.name.father,
+        value: (row) => 'Olanrewaju',
+      },
+      {
+        selector: '#anneAd',
+        // value: (row) => row.name.grand,
+        value: (row) => 'Janet',
+      },
+      {
+        selector: '#uyrukKimlikNo',
+        // value: (row) => row.passportNumber,
+        value: (row) => '2342387423',
+      },
+      {
+        selector: '#eposta',
+        value: (row) => 'no@email.com',
+      },
+      {
+        selector: '#cepTelefon',
+        value: (row) => '5484739847',
+      },
+      {
+        selector:
+          '#body > div > div > section > div > div.content--box--detail > div > div > table > tbody > tr > td > div > div.col-lg-12.controls > table > tbody > tr > td:nth-child(1) > input[type=hidden]:nth-child(2)',
+        value: (row) => true,
+      },
+    ],
+  },
+  {
+    name: 'apply_for_transfer',
+    url: 'https://e-ikamet.goc.gov.tr/Ikamet/UzatmaGecisGiris/GecisBasvuru',
+    regex: 'https://e-ikamet.goc.gov.tr/Ikamet/UzatmaGecisGiris/GecisBasvuru',
+    details: [
+      {
+        selector:
+          '#body > div > div > section > div > div.content--box--detail > div > div > div.form-horizontal.col-lg-12 > div > div.col-lg-8.controls > span > span > span.k-input',
+        value: () => '',
+      },
+      {
+        selector: '#ad',
+        value: () => 'AB',
+      },
+      {
+        selector: '#soyad',
+        value: () => 'AC',
+      },
+      {
+        selector: '#yabanciKimlikNo',
+        value: () => '8493934',
+      },
+      {
+        selector: '#belgeSeri',
+        value: () => 'Residence Permit card serial No.',
+      },
+      {
+        selector: '#belgeNo',
+        value: () => 'Residence Permit Card No.',
+      },
+      {
+        selector:
+          '#body > div > div > section > div > div.content--box--detail > div > div > div:nth-child(5) > div:nth-child(3) > div.col-lg-8.controls > span > span > span.k-input',
+        value: () => 'Country of Nationality',
+      },
+      {
+        selector:
+          '#body > div > div > section > div > div.content--box--detail > div > div > div:nth-child(10) > div.form-horizontal.col-md-6.col-centered > div > div.col-lg-8.controls > span > span > span.k-input',
+        value: () => 'Communication Preference',
+      },
+      {
+        selector: '#iletisim_eMail',
+        value: () => 'Email Address',
+      },
+      {
+        selector: '#iletisim_cepTelefon',
+        value: () => 'Cell Phone',
+      },
+      {
+        selector: '#uzatmaGecisOkudumAnladim',
+        value: () => 'Read and Understood',
+      },
+    ],
+  },
+  {
+    name: 'apply_for_transfer_in_progress',
+    url: 'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris/GecisBasvuru',
+    regex:
+      'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris/GecisBasvuru',
+    details: [
+      {
+        selector: '#basvuruNo',
+        value: () => 'Application No',
+      },
+      {
+        selector: '#cepTelefon',
+        value: () => 'Cell Phone',
+      },
+      {
+        selector: '#ePosta',
+        value: () => 'Email Address',
+      },
+      {
+        selector: '#yabanciKimlikNo',
+        value: () => 'Foreign ID Number',
+      },
+      {
+        selector: '#pasaportBelgeNo',
+        value: () => '#pasaportBelgeNo',
+      },
+    ],
+  },
+  {
+    name: 'apply_for_transfer_in_progress_result',
+    url: 'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris',
+    regex: 'https://e-ikamet.goc.gov.tr/Ikamet/DevamEdenBasvuruGiris',
+    details: [
+      {
+        selector: '#basvuruNo',
+        value: () => 'Application No',
+      },
+      {
+        selector: '#cepTelefon',
+        value: () => 'Cell Phone',
+      },
+      {
+        selector: '#ePosta',
+        value: () => 'Email Address',
+      },
+      {
+        selector: '#yabanciKimlikNo',
+        value: () => 'Foreign ID Number',
+      },
+      {
+        selector: '#pasaportBelgeNo',
+        value: () => '#pasaportBelgeNo',
+      },
+    ],
+  },
+];
+
+async function send(sendData) {
+  data = sendData;
+  page = await util.initPage(config, onContentLoaded);
+  await page.goto(config[0].url, { waitUntil: 'domcontentloaded' });
+}
+
+async function onContentLoaded(res) {
+  counter = util.useCounter(counter);
+  if (counter >= data?.travellers?.length) {
+    util.setCounter(0);
+    if (fs.existsSync(getPath('loop.txt'))) {
+      fs.unlinkSync(getPath('loop.txt'));
+    }
+  }
+  const currentConfig = util.findConfig(await page.url(), config);
+  try {
+    await pageContentHandler(currentConfig);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function pageContentHandler(currentConfig) {
+  const passenger = data.travellers[counter];
+  switch (currentConfig.name) {
+    case 'apply':
+      await util.commit(page, currentConfig.details, data.system);
+      await util.commitCaptchaTokenWithSelector(
+        page,
+        '#CaptchaImage',
+        '#CaptchaInputText',
+        8
+      );
+      await page.click('#btnOnKayitKaydet');
+      break;
+    case 'apply_renewal':
+      await util.commit(page, currentConfig.details, data.system);
+      await util.commitCaptchaTokenWithSelector(
+        page,
+        '#CaptchaImage',
+        '#CaptchaInputText',
+        8
+      );
+      await page.click('#uzatmaGecisGirisBtn');
+    case 'continue_application_renewal':
+      await util.commit(page, currentConfig.details, data.system);
+      await util.commitCaptchaTokenWithSelector(
+        page,
+        '#CaptchaImage',
+        '#CaptchaInputText',
+        8
+      );
+      await page.click(
+        '#body > div > div > section > div > div.content--box--detail > div > div.login-alt > div:nth-child(2) > div > button'
+      );
+      break;
+    case 'result_print_renewal':
+      await util.commit(page, currentConfig.details, data.system);
+      await util.commitCaptchaTokenWithSelector(
+        page,
+        '#CaptchaImage',
+        '#CaptchaInputText',
+        8
+      );
+      await page.click(
+        '#body > div > div > section > div > div.content--box--detail > div > div.login-alt > div:nth-child(2) > div > button'
+      );
+      break;
+    case 'apply_for_transfer':
+      await util.commit(page, currentConfig.details, data.system);
+      await util.commitCaptchaTokenWithSelector(
+        page,
+        '#CaptchaImage',
+        '#CaptchaInputText',
+        8
+      );
+      await page.click('#uzatmaGecisGirisBtn');
+      break;
+    case 'apply_for_transfer_in_progress':
+      await util.commit(page, currentConfig.details, data.system);
+      await util.commitCaptchaTokenWithSelector(
+        page,
+        '#CaptchaImage',
+        '#CaptchaInputText',
+        8
+      );
+      await page.click(
+        '#body > div > div > section > div > div.content--box--detail > div > div.login-alt > div:nth-child(2) > div > button'
+      );
+      break;
+    case 'apply_for_transfer_in_progress_result':
+      await util.commit(page, currentConfig.details, data.system);
+      await util.commitCaptchaTokenWithSelector(
+        page,
+        '#CaptchaImage',
+        '#CaptchaInputText',
+        8
+      );
+      await page.click(
+        '#body > div > div > section > div > div.content--box--detail > div > div.login-alt > div:nth-child(2) > div > button'
+      );
+      break;
+
+    default:
+      break;
+  }
+}
+
+module.exports = { send };
